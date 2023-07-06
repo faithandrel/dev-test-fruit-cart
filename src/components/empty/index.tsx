031bbd654471ab72a9268ed1edc1854e504bc668
Faith Andrel <f.macad@gmail.com>
2023-07-06 23:21:24 +0800

    Fix image

--- src/components/empty/index.tsx
+++ src/components/empty/index.tsx
@@ -1,10 +1,8 @@
-import EmptyImage from '../../assets/empty.png';
-
 export const Empty = () => (
   <div className="flex items-center justify-center empty">
     <div className="text-center">
       <div className="flex justify-center mb-2">
-        <img src={EmptyImage} />
+        <img src={require('../../assets/empty.png')} />
       </div>
       <p>No products have been added.</p>
     </div>
